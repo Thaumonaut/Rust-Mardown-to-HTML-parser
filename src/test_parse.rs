7302d4ea101b2072c7e99a21b795f2a3cff4081d
Jacob Perry <perryjay123@live.com>
2025-11-07 02:45:29 -0800

    Add comment, link, and image support to grammar

--- src/test_parse.rs
+++ src/test_parse.rs
@@ -61,7 +61,7 @@ fn parse_element(pair: pest::iterators::Pair<Rule>) -> String {
             "<hr>\n".to_string()
         },
         _ => {
-            eprintln!("Unhandled rule: {:?}", pair.as_rule());
+            // eprintln!("Unhandled rule: {:?}", pair.as_rule());
             String::new()
         }
     }
@@ -72,24 +72,23 @@ fn parse_heading(pair: pest::iterators::Pair<Rule>) -> String {
   let mut content = String::new();
   for inner_pair in pair.into_inner() {
 
-    match inner_pair.as_rule() {
-        Rule::heading_start => {
-          tag = format!("h{:?}", inner_pair.as_str().len());
-        },
-        Rule:: line_content => {
-          content.push_str(parse_line_content(inner_pair).as_str());
-        },
-        _ => {}
+  match inner_pair.as_rule() {
+      Rule::heading_start => {
+        tag = format!("h{:?}", inner_pair.as_str().len());
+      },
+      Rule:: line_content => {
+        content.push_str(parse_line_content(inner_pair).as_str());
+      },
+      _ => {}
     }
   }
-  html_element_builder(tag, content.as_str())
+  simple_html_element_builder(tag, content.as_str())
 }
 
 fn parse_paragraph(pair: pest::iterators::Pair<Rule>) ->  String {
   let mut content = String::new();
   for inner_pair in pair.into_inner() {
     if inner_pair.as_rule() == Rule::paragraph_line {
-      // println!("{:#?}", inner_pair.clone());
       for nested_pair in inner_pair.into_inner() {
         match nested_pair.as_rule() {
           Rule::line_content => {
@@ -121,7 +120,7 @@ fn parse_blockquote(pair: pest::iterators::Pair<Rule>) -> String {String::from(p
 fn parse_list(pair: pest::iterators::Pair<Rule>) ->       String {String::from(pair.into_inner().as_str())}
 
 
-fn html_element_builder(tag: String, content: &str) -> String {
+fn simple_html_element_builder(tag: String, content: &str) -> String {
     format!("<{t}>{c}</{t}>\n", t = tag, c = content)
 }
 
@@ -136,10 +135,10 @@ fn parse_line_content(pair: pest::iterators::Pair<Rule>) -> String {
                     match content_pair.as_rule() {
                         Rule::italic => {
                             let italic_content = content_pair.into_inner().nth(1).unwrap().as_str();
-                            content.push_str(&html_element_builder("strong".to_string(), &html_element_builder("em".to_string(), italic_content)));
+                            content.push_str(&simple_html_element_builder("strong".to_string(), &simple_html_element_builder("em".to_string(), italic_content)));
                         },
                         _ => {
-                            content.push_str(&html_element_builder("strong".to_string(), &html_element_builder("em".to_string(), content_pair.as_str())));
+                            content.push_str(&simple_html_element_builder("strong".to_string(), &simple_html_element_builder("em".to_string(), content_pair.as_str())));
                         }
                     }
                 }
@@ -153,7 +152,7 @@ fn parse_line_content(pair: pest::iterators::Pair<Rule>) -> String {
                         match item.as_rule() {
                             Rule::italic => {
                                 let italic_content = item.into_inner().nth(1).unwrap().as_str();
-                                bold_html.push_str(&html_element_builder("em".to_string(), italic_content));
+                                bold_html.push_str(&simple_html_element_builder("em".to_string(), italic_content));
                             },
                             Rule::bold_text => {
                                 bold_html.push_str(item.as_str());
@@ -162,21 +161,57 @@ fn parse_line_content(pair: pest::iterators::Pair<Rule>) -> String {
                         }
                     }
                 }
-                content.push_str(&html_element_builder("strong".to_string(), &bold_html));
+                content.push_str(&simple_html_element_builder("strong".to_string(), &bold_html));
             },
             Rule::italic => {
                 let inner_content = inner_pair.into_inner().nth(1).unwrap().as_str();
-                content.push_str(&html_element_builder("em".to_string(), inner_content));
+                content.push_str(&simple_html_element_builder("em".to_string(), inner_content));
             },
             Rule::inline_code => {
                 let inner_content = inner_pair.into_inner().nth(1).unwrap().as_str();
-                content.push_str(&html_element_builder("code".to_string(), inner_content));
+                content.push_str(&simple_html_element_builder("code".to_string(), inner_content));
             },
             Rule::strikethrough => {
                 let inner_content = inner_pair.into_inner().nth(1).unwrap().as_str();
-                content.push_str(&html_element_builder("s".to_string(), inner_content));
+                content.push_str(&simple_html_element_builder("s".to_string(), inner_content));
             },
-            Rule::text => {
+            Rule::inline_link => {
+              let mut tag: (String, String, Option<String>) = (String::new(), String::new(), None);
+              for child in inner_pair.into_inner().flatten() {
+                match child.as_rule() {
+                  Rule::url => {
+                    tag.0.push_str(child.as_str());
+                  },
+                  Rule::title => {
+                    tag.1.push_str(child.as_str());
+                  },
+                  Rule::alt => {
+                    tag.2 = Some(child.as_str().to_string());
+                  },
+                  _ => {}
+                }
+              }
+              content.push_str(format!("<a href=\"{url}\">{title}</a>", url = tag.0, title = tag.1).as_str())
+            },
+            Rule::inline_image => {
+              let mut tag: (String, String, Option<String>) = (String::new(), String::new(), None);
+              for child in inner_pair.into_inner().flatten() {
+                match child.as_rule() {
+                  Rule::url => {
+                    tag.0.push_str(child.as_str());
+                  },
+                  Rule::title => {
+                    tag.2 = Some(child.as_str().to_string());
+                  },
+                  Rule::alt => {
+                    tag.1.push_str(child.as_str());
+                  },
+                  _ => {}
+                }
+              }
+              content.push_str(format!("<img src=\"{url}\" alt=\"{alt}\"/>", url = tag.0, alt = tag.2.unwrap_or_default()).as_str())
+            },
+            Rule::char => {
                 content.push_str(inner_pair.as_str());
             },
             Rule::WHITESPACE => {
--- src/test_simple.md
+++ src/test_simple.md
@@ -1,3 +1,5 @@
-This is **bold** text.
-This is ***bold italic*** text.
-This is **text with *italic* inside** text.
+[link without title](http://example.com)
+[link with title](http://example.com "hover text")
+
+![image without title](http://example.com/image.png)
+![image with title](http://example.com/image.png "Image title")
